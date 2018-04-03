3b69b24f0121e651ee921a59b77f140ed648b377
Byeongkeun Ahn <7p54ks3@naver.com>
2018-04-03 13:06:50 +0900

    Fixed crawling with PhantomJS

--- crawl.py
+++ crawl.py
@@ -1,38 +1,42 @@
 import urllib.request
 import re
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
+_caps = DesiredCapabilities.PHANTOMJS
+_caps["phantomjs.page.settings.userAgent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36"
+_my_driver = webdriver.PhantomJS(service_args=['--proxy-type=none'])
 def download(url):
-    req = urllib.request.Request(
-        url, 
-        data=None, 
-        headers={
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
-        }
-    )
-    return urllib.request.urlopen(req).read().decode('utf-8')
-def querySong(id):
+    _my_driver.get(url)
+    return_val = _my_driver.page_source
+    return return_val
+def querySong(id, myfile):
     url = 'https://www.melon.com/song/detail.htm?songId=%s' % (id)
     data = download(url)
     regex = re.compile(r'<div class="lyric" id="d_video_summary">.*\r*\n[ \t]*([^\r\n]+)')
     lyrics_list = regex.findall(data)
     if not lyrics_list:
-        nolyrics_list = re.compile(r'<div class="lyric_none">').findall(data)
-        if nolyrics_list:
-            print('Lyrics not available yet')
-        else:
-            print('Cannot parse lyrics HTML')
+        #nolyrics_list = re.compile(r'<div class="lyric_none">').findall(data)
+        #if nolyrics_list:
+            #print('Lyrics not available yet')
+        #else:
+            #print('Cannot parse lyrics HTML')
         return
     lyrics = lyrics_list[0]
-    regex = re.compile(r'(.+?)<BR>')
-    lines = regex.findall(lyrics)
+    regex = re.compile(r'(.+?)<[Bb][Rr]>')
+    lines = regex.findall(lyrics)    
+    myfile.write(id + '\n')
     for line in lines:
-        print(line)
+        myfile.write(line + '\n')
+    myfile.write('\n')
 
-url = 'https://www.melon.com/genre/song_list.htm?gnrCode=GN0300#params%5BgnrCode%5D=GN0300&params%5BdtlGnrCode%5D=&params%5BorderBy%5D=NEW&params%5BsteadyYn%5D=N&po=pageObj&startIndex=1'
-text = download(url)
-regex = re.compile(r"javascript:melon\.link\.goSongDetail\('(\d+)'\)")
-nums = regex.findall(text)
-for i in nums:
-    print(i)
-    querySong(i)
-    print('')
+url_base = 'https://www.melon.com/genre/song_list.htm?gnrCode=GN0300#params%5BgnrCode%5D=GN0300&params%5BdtlGnrCode%5D=&params%5BorderBy%5D=NEW&params%5BsteadyYn%5D=N&po=pageObj&startIndex='
+
+for i in range(1, 1000, 50):
+    with open("test" + str(i) + ".txt", "a", encoding='UTF8') as myfile:
+        text = download(url_base + str(i))
+        regex = re.compile(r"javascript:melon\.link\.goSongDetail\('(\d+)'\)")
+        nums = regex.findall(text)
+        for j in nums:
+            print("Song number " + str(j) + " processed")
+            querySong(j, myfile)
