34059da3f44791dfdc600f5817df9cac5c2d0d8a
Byeongkeun Ahn <7p54ks3@naver.com>
2018-05-01 01:46:06 +0900

    Implement vowel scoring in (Hahirjee, 2009)

--- rhyme_density.py
+++ rhyme_density.py
@@ -3,8 +3,8 @@ import numpy as np
 
 # a, b: 각 모음의 출현 빈도수
 def similarity_score(a, b):
-    a = min(a, 3)
-    b = min(b, 3)
+    #a = min(a, 3)
+    #b = min(b, 3)
     return a * b
 
 # 모음 20가지에 대해 라임 유사도 분석 실시
@@ -18,22 +18,34 @@ def analyze(input_str):
         vowel = hangul.decompose_hangul(c)[1]
         index = hangul.joongseongs.index(vowel)
         rhyme_counts[index] += 1
-    mat = np.zeros((vowel_count, vowel_count))
+    counts = np.zeros((vowel_count, vowel_count))
     for i in range(vowel_count):
         for j in range(i, vowel_count):
-            mat[i][j] = similarity_score(rhyme_counts[i], rhyme_counts[j])
+            counts[i][j] = similarity_score(rhyme_counts[i], rhyme_counts[j])
+    total_count = vowel_count * (vowel_count + 1) / 2
+    totals = np.ones((vowel_count, vowel_count)) * total_count
+    return counts, totals
+
+def analyze_file(filename):
+    with open(filename, 'r', encoding='utf8') as myfile:
+        data = myfile.readlines()
+
+    vowel_count = len(hangul.joongseongs)
+    counts = np.zeros((vowel_count, vowel_count))
+    totals = np.zeros((vowel_count, vowel_count))
+    for i in range(0, len(data)-1):
+        counts_cur, totals_cur = analyze(data[i] + ' ' + data[i+1])
+        counts += counts_cur
+        totals += totals_cur
+        if i % 200 == 0 or i == len(data)-1:
+            print('Processed %dth line' % int(i))
+    mat = counts / totals
     return mat
 
-#filename = 'corpus2\\rhyme_match.txt'
-filename = 'corpus2\\output_100000.txt'
-with open(filename, 'r', encoding='utf8') as myfile:
-    data = myfile.readlines()
+file_rhyme = 'corpus2\\rhyme_match.txt'
+file_random = 'corpus2\\output_100000.txt'
+mat_rhyme = analyze_file(file_rhyme)
+mat_random = analyze_file(file_random)
+odds = np.log(mat_rhyme / (mat_random + 0.000000000000001))
 
-vowel_count = len(hangul.joongseongs)
-mat = np.zeros((vowel_count, vowel_count))
-for i in range(0, len(data)-1):
-    mat += analyze(data[i] + ' ' + data[i+1])
-    if i % 100 == 0:
-        print('Processed %dth line' % int(i))
-
-print(mat)
+print(odds)
